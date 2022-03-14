4d865f6480fd41dffb1fa8b2f2ebd316bd765dbb
finalLaugh <futureisahead@gmail.com>
2022-03-14 21:37:21 +0200

    Added other methods for selecting page elements.

--- src/test/java/pages/homepage/HomePage.java
+++ src/test/java/pages/homepage/HomePage.java
@@ -3,6 +3,7 @@ package pages.homepage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pages.BasePage;
@@ -12,6 +13,10 @@ public class HomePage extends BasePage {
     public static HomePage instance;
 
     private By logo = By.id("logo");
+    private By signInButton = By.id("btn1");
+    private By skipSignInButton = By.id("btn2");
+    private By emailField = By.id("email");
+    private By nextButton = By.id("enterimg");
 
     private HomePage() {
 
@@ -27,5 +32,30 @@ public class HomePage extends BasePage {
     public boolean checkLogo() {
         LOG.info("Check if Logo is displayed");
         return driver.findElement(logo).isDisplayed();
+    }
+
+    public void clickSignInButton() {
+        LOG.info("Click Sign in button");
+        driver.findElement(signInButton).click();
+    }
+
+    public void clickSkipSignInButton() {
+        LOG.info("Click skip Sign in button");
+        driver.findElement(skipSignInButton).click();
+    }
+
+    public void clickBack () {
+        LOG.info("Go back");
+        driver.navigate().back();
+    }
+
+    public void checkEmailField(String email) {
+        LOG.info("Put email into the field");
+        driver.findElement(emailField).sendKeys(email);
+    }
+
+    public void clickSubmitButton() {
+        LOG.info("Click submit");
+        driver.findElement(nextButton).click();
     }
 }
--- src/test/java/test/homepageTest/HomePageTest.java
+++ src/test/java/test/homepageTest/HomePageTest.java
@@ -1,5 +1,6 @@
 package test.homepageTest;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import test.BaseTest;
@@ -7,7 +8,14 @@ import test.BaseTest;
 public class HomePageTest extends BaseTest {
 
     @Test
-    public void testLogoIsDisplayed() {
+    public void testLogoIsDisplayed() throws InterruptedException {
         Assert.assertTrue(homePage.checkLogo(), "Logo is not shown");
+        //homePage.clickSignInButton();
+       // homePage.clickBack();
+        //homePage.clickSkipSignInButton();
+        homePage.checkEmailField("ffxii23@yahoo.com");
+        Thread.sleep(2000);
+        homePage.clickSubmitButton();
+        Thread.sleep(2000);
     }
 }
